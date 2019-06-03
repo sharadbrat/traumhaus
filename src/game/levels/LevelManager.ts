a665ebed29a31527354dcdd6a31666c2a1271ce6
sharadbrat <sharoobrazno@mail.ru>
2019-06-03 15:42:00 +0200

    added stair level

--- src/game/levels/LevelManager.ts
+++ src/game/levels/LevelManager.ts
@@ -1,13 +1,15 @@
 import { LEVEL_1_DATA } from './level-1';
-import { LEVEL_2_DATA } from './level-2';
+import { LEVEL_3_DATA } from './level-3';
 import { LEVEL_TEST_DATA } from './level-test';
 import { LevelMapData } from '../entities/model';
+import { LEVEL_2_DATA } from './level-2';
 
 export class LevelManager {
 
   private static readonly LEVELS = {
     [LEVEL_1_DATA.id]: LEVEL_1_DATA,
     [LEVEL_2_DATA.id]: LEVEL_2_DATA,
+    [LEVEL_3_DATA.id]: LEVEL_3_DATA,
     [LEVEL_TEST_DATA.id]: LEVEL_TEST_DATA,
   };
 
--- src/game/levels/index.ts
+++ src/game/levels/index.ts
@@ -1,3 +1,3 @@
 export * from './level-1';
-export * from './level-2';
+export * from './level-3';
 export * from './LevelManager';

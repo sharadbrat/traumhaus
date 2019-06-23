519685081ff9c4b27a5160f032142e795ef7e96a
sharadbrat <sharoobrazno@mail.ru>
2019-06-23 12:27:34 +0200

    removed clear progress from settings menu in game

--- src/components/SettingsMenu/SettingsMenu.tsx
+++ src/components/SettingsMenu/SettingsMenu.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 import { Menu } from '../Menu';
-import { SettingsForm } from '../SettingsForm';
 
 import './SettingsMenu.scss';
 
@@ -12,10 +11,27 @@ interface SettingsMenuProps {
 
 export class SettingsMenu extends React.Component<SettingsMenuProps> {
 
+  private onFullscreenButtonClick() {
+    const root = document.getElementById('root');
+    if (root) {
+      root.requestFullscreen().catch((e: Error) => {
+        alert(`Could not start fullscreen mode!\nError message: ${e.message}`);
+      });
+    }
+  };
+
   render() {
     return (
       <Menu heading="Settings" isActive={this.props.isActive} index={1001}>
-        <SettingsForm/>
+        <div className="settings__block">
+
+          <h2 className="settings__subheading">Display</h2>
+          <div className="settings__group">
+            <span className="settings__label">Fullscreen</span>
+            <button className="settings__button" onClick={this.onFullscreenButtonClick}>Enable</button>
+          </div>
+
+        </div>
         <button className="settings__back" onClick={this.props.onClose}>Back to menu</button>
       </Menu>
     );
